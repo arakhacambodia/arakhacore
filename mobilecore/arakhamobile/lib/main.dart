353962a470ea1fce6f9206630d7d80616df53063
Arun <arunlam@outlook.com>
2024-07-15 14:37:09 +0700

    responsive font size to screen width

--- mobilecore/arakhamobile/lib/main.dart
+++ mobilecore/arakhamobile/lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
 void main() {
+  initializeDateFormatting('km', '');
   runApp(const HomePage());
 }
 
@@ -15,61 +17,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late String dateString;
   late String timeString;
 
   @override
   void initState() {
     super.initState();
-    timeString = _formatDateTime(DateTime.now());
+    dateString = _formatDate(DateTime.now());
+    timeString = _formatTime(DateTime.now());
     Timer.periodic(const Duration(seconds: 1), (Timer t) => _getTime());
   }
 
   void _getTime() {
-    final String formattedDateTime = _formatDateTime(DateTime.now());
+    final String formattedDate = _formatDate(DateTime.now());
+    final String formattedTime = _formatTime(DateTime.now());
     setState(() {
-      timeString = formattedDateTime;
+      dateString = formattedDate;
+      timeString = formattedTime;
     });
   }
 
-  String _formatDateTime(DateTime dateTime) {
-    return DateFormat('yyyy-MM-dd HH:mm:ss a').format(dateTime);
+  String _formatTime(DateTime dateTime) {
+    return DateFormat('hh:mm:ss a').format(dateTime);
+  }
+
+  String _formatDate(DateTime dateTime) {
+    return DateFormat('yyyy-MM-dd (EEEE)', 'km').format(dateTime);
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    double width = size.width;
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                "Arakha Web...",
-                style: TextStyle(fontSize: 48, color: Colors.green),
-              ),
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          dateString,
+                          style: TextStyle(fontSize: width * 0.1),
+                        ),
+                        Text(
+                          timeString,
+                          style: TextStyle(fontSize: width * 0.08),
+                        ),
+                      ],
+                    )),
+              ],
             ),
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        timeString.substring(0, 10),
-                        style: const TextStyle(fontSize: 48),
-                      ),
-                      Text(
-                        timeString.substring(11),
-                        style: const TextStyle(fontSize: 48),
-                      ),
-                    ],
-                  )),
-            ],
           ),
         ),
       ),
