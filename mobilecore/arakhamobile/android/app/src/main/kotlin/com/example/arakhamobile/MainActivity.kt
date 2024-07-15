7326943d4897983ddeccf950191aab1a70959cb7
Arun <arunlam@outlook.com>
2024-07-15 11:12:36 +0700

    init web with firebase config

---
+++ mobilecore/arakhamobile/android/app/src/main/kotlin/com/example/arakhamobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.arakhamobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mobilecore/arakhamobile/lib/main.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late String timeString;
+
+  @override
+  void initState() {
+    super.initState();
+    timeString = _formatDateTime(DateTime.now());
+    Timer.periodic(const Duration(seconds: 1), (Timer t) => _getTime());
+  }
+
+  void _getTime() {
+    final String formattedDateTime = _formatDateTime(DateTime.now());
+    setState(() {
+      timeString = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('yyyy-MM-dd HH:mm:ss a').format(dateTime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                'Arakha Web',
+                style: TextStyle(fontSize: 48, color: Colors.green),
+              ),
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        timeString.substring(0, 10),
+                        style: const TextStyle(fontSize: 48),
+                      ),
+                      Text(
+                        timeString.substring(11),
+                        style: const TextStyle(fontSize: 48),
+                      ),
+                    ],
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
